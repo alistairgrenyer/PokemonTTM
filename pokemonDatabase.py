ffa1a2c822d2528e09abfa3f9d9c9a0f15987091
alistairgrenyer <alistair.grenyer@capgemini.com>
2022-10-17 16:01:54 +0100

    commit 2

--- pokemonDatabase.py
+++ pokemonDatabase.py
@@ -1,13 +1,16 @@
 import sqlite3
 import pandas as pd
+import logging
+
+logging.basicConfig(filename='pokemonDatabase.log', filemode='w', level=logging.DEBUG)
 
 
-def databaseConnect():
+def databaseConnect() -> sqlite3.Connection:
     try:
         conn = sqlite3.connect('Pokemon.db')
         return conn
     except KeyError as e:
-        print(e)
+        logging.info(e)
 
 
 def createTable(conn: sqlite3.Connection):
@@ -19,23 +22,48 @@ def createTable(conn: sqlite3.Connection):
             "Artwork"	TEXT,
             "Attack"	INTEGER,
             "Defence"	INTEGER,
-            "Type1"	TEXT,
-            "Type2"	TEXT
+            "Type"	TEXT,
+            PRIMARY KEY("Name")
             );
             '''
         cursor.execute(createPokemonDatabase)
         conn.commit()
     except sqlite3.OperationalError as e:
-        print(e)
+        logging.info(e)
 
 
-def findPokemonFromName(conn: sqlite3.Connection, pokemonName: str):
+def commitToDatabase(conn: sqlite3.Connection, command: str) -> sqlite3.Connection.cursor:
+    cursor = conn.cursor()
+    try:
+        cursor.execute(command)
+        conn.commit()
+    except sqlite3.IntegrityError as e:
+        logging.info(e)
+    return cursor
+
+
+def findPokemonFromName(conn: sqlite3.Connection, pokemonName: str) -> dict:
     selectData = f'''
         SELECT * FROM PokemonDatabase
-        WHERE PokemonDatabase.Name = "{pokemonName}"
+        WHERE Name = "{pokemonName}"
         '''
-    pokemonNameDataFrame = pd.read_sql_query(selectData, conn)
-    if pokemonNameDataFrame.empty:
-        return None
-    else:
-        return pokemonNameDataFrame
+    pokemonNameDF = pd.read_sql_query(selectData, conn)
+    pokemon = {"Name": pokemonNameDF["Name"][0], "Artwork": pokemonNameDF["Artwork"][0],
+               "Attack": pokemonNameDF["Attack"][0], "Defence": pokemonNameDF["Defence"][0],
+               "Type1": pokemonNameDF["Type1"][0], "Type2": pokemonNameDF["Type2"][0]}
+    return pokemon
+
+
+def addPokemonToDatabase(conn: sqlite3.Connection, pokemonData: list):
+    for pokemon in pokemonData:
+        try:
+            bookData = f'''
+                    INSERT INTO PokemonDatabase
+                    (Name, Artwork, Attack, Defence, Type1, Type2)
+                    VALUES ('{pokemon["Name"]}', '{pokemon["Artwork"]}', 
+                    '{pokemon["Attack"]}', '{pokemon["Defence"]}', 
+                    '{pokemon["Type1"]}', '{pokemon["Type2"]}')
+                    '''
+            commitToDatabase(conn, bookData)
+        except KeyError as e:
+            logging.info(e)
