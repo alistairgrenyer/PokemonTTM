dbf993dbacb19839a9c1ff532f4f44f5f196c95f
Noctiscreo <noctiscreo@gmail.com>
2022-10-18 13:14:34 +0100

    Began intergrating data.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     # Get list of pokemon from database.
-    pokemonList = ["Poke1"]
+    pokemonList = []
     return render_template("index.html", pokemonList=pokemonList)
 
 @app.route("/downloadPokemon")
