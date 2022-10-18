7dd475a4af14e409dcacbfeed038de64f1e2d8d9
alistairgrenyer <alistair.grenyer@capgemini.com>
2022-10-18 18:07:27 +0100

    first commit of updated logs

--- logs.py
+++ logs.py
@@ -1,4 +1,21 @@
 import logging
-logging.basicConfig(filename="pokemonLogs.log", filemode="a", level=logging.DEBUG,
-                    format='%(pastime)s - %(levelness)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-logger = logging.getLogger("logs")
+
+
+def clearLogs():
+    with open('pokemonLogs.log', 'w'):
+        pass
+
+
+class Logger(object):
+    _instance = None
+
+    def __init__(self):
+        self.logger = logging.getLogger()
+
+    def __new__(cls):
+        if cls._instance is None:
+            print('Creating the object')
+            cls._instance = super(Logger, cls).__new__(cls)
+            logging.basicConfig(filename="pokemonLogs.log", filemode="a", level=logging.DEBUG,
+                                format='%(asctime)s - %(levelname)s: %(message)s [%(filename)s:%(lineno)s - %(funcName)20s() ]', datefmt='%m/%d/%Y %I:%M:%S %p')
+        return cls._instance
--- pokemonDatabase.py
+++ pokemonDatabase.py
@@ -1,21 +1,20 @@
 import sqlite3
 import pandas as pd
-
 import logs
-import logging
-
-logger = logging.getLogger("logs.pokemonDatabase")
 
 
 def databaseConnect() -> sqlite3.Connection:
+    databaseConnectLogs = logs.Logger()
     try:
         conn = sqlite3.connect('Pokemon.db')
         return conn
-    except KeyError as e:
-        logger.error(e)
+    except Exception as e:
+        databaseConnectLogs.logger.error(e)
 
 
-def createTable(conn: sqlite3.Connection):
+def createTable():
+    createTableLogs = logs.Logger()
+    conn = databaseConnect()
     cursor = conn.cursor()
     try:
         createPokemonDatabase = '''
@@ -32,25 +31,31 @@ def createTable(conn: sqlite3.Connection):
         cursor.execute(createPokemonDatabase)
         conn.commit()
     except sqlite3.OperationalError as e:
-        logger.error(e)
+        createTableLogs.logger.error(e)
+    conn.close()
 
 
 def commitToDatabase(conn: sqlite3.Connection, command: str) -> sqlite3.Connection.cursor:
+    commitToDatabaseLogs = logs.Logger()
     cursor = conn.cursor()
     try:
         cursor.execute(command)
         conn.commit()
     except sqlite3.IntegrityError as e:
-        logger.error(e)
+        commitToDatabaseLogs.logger.error(e)
     return cursor
 
 
 def findPokemonFromName(conn: sqlite3.Connection, pokemonName: str) -> dict:
+    findPokemonFromNameLogs = logs.Logger()
     selectData = f'''
         SELECT * FROM PokemonDatabase
         WHERE Name = "{pokemonName}"
         '''
     pokemonNameDF = pd.read_sql_query(selectData, conn)
+    if pokemonNameDF.empty:
+        findPokemonFromNameLogs.logger.info(f"No pokemon with name: {pokemonName}")
+
     pokemon = {"Name": pokemonNameDF["Name"][0], "Artwork": pokemonNameDF["Artwork"][0],
                "Attack": pokemonNameDF["Attack"][0], "Defence": pokemonNameDF["Defence"][0],
                "Type1": pokemonNameDF["Type1"][0], "Type2": pokemonNameDF["Type2"][0]}
@@ -58,6 +63,7 @@ def findPokemonFromName(conn: sqlite3.Connection, pokemonName: str) -> dict:
 
 
 def addPokemonToDatabase(conn: sqlite3.Connection, pokemonData: list):
+    addPokemonToDatabaseLogs = logs.Logger()
     for pokemon in pokemonData:
         try:
             pokemonInsertSql = f'''
@@ -68,19 +74,25 @@ def addPokemonToDatabase(conn: sqlite3.Connection, pokemonData: list):
                     '{pokemon["Type1"]}', '{pokemon["Type2"]}')
                     '''
             commitToDatabase(conn, pokemonInsertSql)
-        except KeyError as e:
-            logger.error(e)
+        except Exception as e:
+            addPokemonToDatabaseLogs.logger.error(e)
 
 
 def findAllPokemon(conn: sqlite3.Connection) -> list:
+    findAllPokemonLogs = logs.Logger()
     selectData = f'''
         SELECT * FROM PokemonDatabase
         '''
     allPokemonDF = pd.read_sql_query(selectData, conn)
+    if allPokemonDF.empty:
+        findAllPokemonLogs.logger.info("No pokemon in database")
     allPokemonList = []
     for row in allPokemonDF.itertuples():
-        pokemon = {"Name": row.Name, "Artwork": row.Artwork,
-                   "Attack": row.Attack, "Defence": row.Defence,
-                   "Type1": row.Type1, "Type2": row.Type2}
-        allPokemonList.append(pokemon)
+        try:
+            pokemon = {"Name": row.Name, "Artwork": row.Artwork,
+                       "Attack": row.Attack, "Defence": row.Defence,
+                       "Type1": row.Type1, "Type2": row.Type2}
+            allPokemonList.append(pokemon)
+        except Exception as e:
+            findAllPokemonLogs.logger.error(e)
     return allPokemonList
